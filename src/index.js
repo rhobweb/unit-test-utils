d600c0ec22ceba06f8ad340b160eab29ca643327
Rhob Weber <rhobweb@hotmail.com>
2023-06-08 08:43:27 +0100

    Reset to initial version and publish to npm.

---
+++ src/index.js
@@ -0,0 +1,117 @@
+/**
+ * To enable full error output from chai, do the following in the test:
+ *    const chai          = require( 'chai' );
+ *    const { expect }    = chai;
+ *    chai.config.truncateThreshold = 0; // Do not truncate errors
+ */
+'use strict';
+
+const sinon  = require( 'sinon' );
+const rewire = require( 'rewire' );
+
+// To enable full error output from chai, enable the following here or in the test
+// const chai          = require( 'chai' );
+// chai.config.truncateThreshold = 0; // Do not truncate errors
+
+class UnitTestUtils {
+
+  constructor( { testModulePathname, arrUnreqire = [] } ) {
+    this.testModulePathname = testModulePathname;
+    this.sandbox            = null;
+    this.arrUnrequire       = [ testModulePathname, ...arrUnreqire ].map( e => require.resolve( e ) );
+  }
+
+  /**
+   * Modules load other modules, so to force a module reload need to delete
+   * the test module and all child modules from the require cache.
+   */
+  unrequireModules() {
+    this.arrUnrequire.forEach( key => {
+      delete require.cache[ key ];
+    } );
+  }
+  
+  commonBeforeEach() {
+    this.unrequireModules();
+    this.sandbox = sinon.createSandbox();
+  }
+  
+  commonAfterEach() {
+    this.sandbox.restore();
+    this.unrequireModules();
+  }
+
+  createTestModule() {
+    const testModule = rewire( this.testModulePathname );
+    return testModule;
+  }
+  
+  getProps( testModule, arrProp = [] ) {
+    const testProps = {};
+    arrProp.forEach( m => { 
+      testProps[ m ] = testModule.__get__( m );
+    } );
+    return testProps;
+  }
+
+  getPrivateStubs( testModule, arrFnName ) {
+    const testStubs   = {};
+    const testDummies = {};
+  
+    arrFnName.forEach( fnName => {
+      testStubs[ fnName ]   = () => {};
+      testDummies[ fnName ] = ( ...args ) => testStubs[ fnName ]( ...args );
+      testModule.__set__( fnName, testDummies[ fnName ] );
+    } );
+  
+    return testStubs;
+  }
+
+  getPrivateStub( testModule, className ) {
+    const testStubs = this.getPrivateStubs( testModule, [ className ] );
+    return testStubs[ className ];
+  }
+
+  /**
+   * Note: If faking the constructor return value, it must be a non-primitive, i.e., an object,
+   *       see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new
+   * @param {*} testModule 
+   * @param {*} arrClassName 
+   * @returns object with properties being the class names and values being the stubbed constructors.
+   */
+  getPrivateConstructorStubs( testModule, arrClassName ) {
+    const testStubs = {};
+  
+    arrClassName.forEach( className => {
+      const constructorStub = this.sandbox.stub();
+      class testClass {
+        constructor ( ...args ) {
+          return constructorStub( ...args );
+        }
+      }
+      testStubs[ className ] = constructorStub;
+      testModule.__set__( className, testClass );
+    } );
+  
+    return testStubs;
+  }
+
+  getPrivateConstructorStub( testModule, className ) {
+    const testStubs = this.getPrivateConstructorStubs( testModule, [ className ] );
+    return testStubs[ className ];
+  }
+  
+  /**
+   * @param {Object} with properties:
+   * @returns object with properties:
+   */
+  createTestComponents( { propNames = [], fnNames = [], classNames = [] } = {} ) {
+    const testModule   = this.createTestModule();
+    const testProps    = this.getProps( testModule, propNames );
+    const testStubs    = this.getPrivateStubs( testModule, fnNames );
+    const testConStubs = this.getPrivateConstructorStubs( testModule, classNames );
+    return { testModule, testProps, testStubs, testConStubs };
+  }
+}
+
+module.exports = UnitTestUtils;
